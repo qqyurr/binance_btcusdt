caedd8037923de3b9e6167f3f18e9c0aae039244
gyuyeon <byyulli16@gmail.com>
2024-12-11 14:17:13 +0900

    setting: package.json, global.css, tsconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,14 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        "binance-green": "#22c55e", // 상승 시 색상
+        "binance-red": "#ef4444", // 하락 시 색상
+        "binance-bg": "#0f172a", // Binance 어두운 테마 배경
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
